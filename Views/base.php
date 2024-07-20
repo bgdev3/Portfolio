eb11f43eae8a5e1ccc51a50fcf097000a3596a92
GuiMix <boukehaili.g@gmail.com>
2024-07-20 14:33:40 +0200

    update files

--- Views/base.php
+++ Views/base.php
@@ -9,26 +9,28 @@
     <title>Portfolio</title>
 </head>
 <body>
-    <header class="header">
-        <h1>Portfolio</h1>
-        <nav>
-            <ul class="nav-header">
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="#">Productions</a></li>
-                <li><a href="#">Contact</a></li>
+    <div class="wrapper">
+        <header class="header">
+            <h1>Portfolio</h1>
+            <nav>
+                <ul class="nav-header">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="#">Productions</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main><?= $content ?></main>
+        <footer>
+            <ul>
+                <!-- facebook -->
+                <li><a href="https://www.facebook.com/boo.ka.5"><i class="fa-brands fa-facebook-f"></i></a></li>
+                <!-- LinkedIn -->
+                <li><a href="www.linkedin.com/in/guillaume-boukehaili-919656238"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                <!-- Github -->
+                <li><a href="github.com/charlieGui"><i class="fa-brands fa-github"></i></a></li>
             </ul>
-        </nav>
-    </header>
-    <main><?= $content ?></main>
-    <footer>
-        <ul>
-            <!-- facebook -->
-            <li><a href="https://www.facebook.com/boo.ka.5"><i class="fa-brands fa-facebook-f"></i></a></li>
-            <!-- LinkedIn -->
-            <li><a href="www.linkedin.com/in/guillaume-boukehaili-919656238"><i class="fa-brands fa-linkedin-in"></i></a></li>
-            <!-- Github -->
-            <li><a href="github.com/charlieGui"><i class="fa-brands fa-github"></i></a></li>
-        </ul>
-    </footer>
+        </footer>
+    </div>   
 </body>
 </html>
