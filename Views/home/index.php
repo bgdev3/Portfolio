c1be3aba12f2f384be0e4a7217263aa7c5645e25
GuiMix <boukehaili.g@gmail.com>
2024-07-21 22:37:12 +0200

    update files

--- Views/home/index.php
+++ Views/home/index.php
@@ -5,6 +5,10 @@ $title = 'Accueil';
 <section>
     <div>
         <p>Guillaume<Br>Boukehaili<br>Développeur web</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aut facilis mollitia et, nisi, officiis commodi consequatur dicta, omnis inventore dolor ut. Tenetur sequi, commodi perspiciatis earum iusto iste voluptatem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repellendus rem corporis numquam harum! Fuga illum nemo, nam earum rem, porro nesciunt, error delectus fugit consequuntur non inventore magnam culpa?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptates ipsum possimus non nostrum, soluta dolorum quod. Suscipit quasi ex eius consequatur veniam earum molestiae nemo cum, iste, autem quis!
+        Laborum illo blanditiis aperiam sapiente, sed laboriosam asperiores impedit maiores, omnis eum minus sint commodi necessitatibus at aspernatur deserunt! Quisquam non voluptatum et alias sunt ea officiis magni explicabo dolores?
+        Et commodi ab reprehenderit corporis dolorum eligendi mollitia. Officiis perferendis atque illo itaque obcaecati expedita, temporibus nam dolorem rerum, vel amet? Sed maxime repudiandae deserunt perferendis velit iste ipsum dignissimos!
+        Voluptas assumenda deserunt officiis ipsum architecto inventore amet repellendus, expedita suscipit rem voluptatum? Nisi aspernatur itaque sit nam ut quo, aut voluptatem nesciunt deserunt, earum repudiandae non numquam, at quisquam.Inventore perferendis consequuntur illum suscipit magnam vel laborum incidunt maiores cum quo, perspiciatis facilis. Doloremque, quod accusantium? Voluptas dolorem, dolorum illum, tempora quam officia unde dolor expedita animi, consequuntur incidunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus illum animi harum corrupti quidem voluptatum exercitationem, molestiae quam possimus corporis. Deleniti, officia. Nisi repudiandae dolore deleniti architecto, nostrum iste? Harum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aut facilis mollitia et, nisi, officiis commodi consequatur dicta, omnis inventore dolor ut. Tenetur sequi, commodi perspiciatis earum iusto iste voluptatem.</p>
     </div>
 </section>
