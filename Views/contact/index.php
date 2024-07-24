9c89e4c51c0c6fba5141f9e19656aa3e005d311b
GuiMix <boukehaili.g@gmail.com>
2024-07-24 18:28:19 +0200

    update contact form

--- Views/contact/index.php
+++ Views/contact/index.php
@@ -3,32 +3,40 @@ $title = 'Contact';
 ?>
 
 <section>
-    <form action="">
-        <ul>
-            <li>
-                <label for="name">Nom:</label>
-                <input type="text" id="name" name="name">
-            </li>
-            <li>
-                <label for="surname">Prénom:</label>
-                <input type="text" id="surname" name="surname">
-            </li>
-            <li>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email">
-            </li>
-            <li>
-                <label for="phone">Téléphone:</label>
-                <input type="text" id="phone" name="phone">
-            </li>
-            <li>
-                <label for="message">Message</label>
-                <input type="textarea" id="message" name="message">
-            </li>
-            <input type="submit" value="Envoyer">
+    <div  class="form-contact">
+        <h1>Contact</h1>
+        
+        <form action="" method="POST" novalidate>
+        <small>(*)Champs obligatoire</small>
+            <div class="flex-form">
+                <div>
+                    <label for="name">Nom*:</label>
+                    <input type="text" id="name" name="name" required>
+                
+                    <label for="surname">Prénom*:</label>
+                    <input type="text" id="surname" name="surname" required>
+                
+                    <label for="email">Email*:</label>
+                    <input type="email" id="email" name="email" required>
+                
+                    <label for="phone">Téléphone*:</label>
+                    <input type="text" id="phone" name="phone" required>
+                </div>
+
+                <div>
+                    <label for="obj">Object*</label>
+                    <input type="text" id="obj" name="obj" required>
+
+                    <label for="message">Message*</label>
+                    <textarea id="message" name="message" rows="8" required></textarea>
+                
+                </div>
+            </div>
+        
+            <input type="submit" class="btn" value="Envoyer">
         
         
-        </ul>
-        
-    </form>
+        </form>
+    </div>
+   
 </section>
